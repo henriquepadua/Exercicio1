e431de4207f2ce442d06037d641f7ada5cd92645
henrique <henrique.lommes@gmail.com>
2021-09-05 16:23:23 -0300

    arquivo soma em java

---
+++ src/Somar.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+ class Somar {
+	 public static void main(String[] args) {
+		 Scanner sc=new Scanner(System.in);
+		 
+		 //Declarando variaveis
+		 int num1,num2,soma;
+
+		 //Leituras
+		 System.out.println("Digite um Inteiro");
+		 num1=sc.nextInt();
+		 
+		 System.out.println("Digite outro Inteiro");
+		 num2=sc.nextInt();
+		 
+		 //Somar 
+		 soma=num1+num2;
+		 
+		 //Mostrar na tela
+		 System.out.println("Soma\n"+soma);
+	 }
+}
+ 
+//Minha autoaliação nesta atividade eh 5,pois entendi tudo que era para ser feito e executar o mesmo
